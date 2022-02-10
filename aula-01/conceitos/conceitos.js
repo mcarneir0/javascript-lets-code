70f3bc5b24ae04273752b8d3f580f7eb5697a0e9
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-02-09 21:15:05 -0300

    Adicionado Conceitos da Aula 01

---
+++ aula-01/conceitos/conceitos.js
@@ -0,0 +1,23 @@
+// Javascript - Linguagem de Programação
+
+// Armazenar Dados
+
+// Sintaxe
+// var chave = valor
+// let chave = valor
+// const chave = valor
+
+var escola = "Let's Code";
+let aluno = "Matheus";
+const idade = 23;
+var alto = true;
+
+
+// Executar Tarefas
+// Sintaxe
+// comando();
+// comando(parametro);
+// comando(param1, param2);
+prompt();
+prompt("Digite seu nome");
+prompt("Digite sua idade", "Sua idade aqui");
