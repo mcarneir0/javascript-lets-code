627e89e31b4a437047e122f50cce4622b8d4348e
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-02-09 22:21:51 -0300

    Adicionado Programa Agenda

---
+++ aula-01/agenda/programa.js
@@ -0,0 +1,19 @@
+// Armazenando os dados
+
+// Entrada de dados
+var nome = prompt("Digite seu nome:");
+var email = prompt("Digite seu email:");
+var telefone = prompt("Digite seu telefeone:")
+
+
+// Executando instruções
+
+// Saída de dados no console
+console.log("nome:", nome);
+console.log("email:", email);
+console.log("telefone:", telefone);
+
+// Saída de dados na tela
+document.write("<hr><p>", "Nome: ", nome, "</p>");
+document.write("<p>", "Email: ", email, "</p>");
+document.write("<p>", "Telefone: ", telefone, "</p><hr>");
