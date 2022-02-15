7925a66139c146a6c7138ac4e238a47b509df348
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-02-14 21:54:14 -0300

    Adicionado Aula 03 - Operadores Lógicos

---
+++ aula-03/operadores.js
@@ -0,0 +1,28 @@
+// Operadores Aritiméticos
+
+console.log("=== OPERADORES ARITIMÉTICOS ===");
+console.log("+", "Adição");
+console.log("-", "Subtração");
+console.log("*", "Multiplicação");
+console.log("/", "Divisão");
+console.log("%", "Resto da Divisão");
+
+
+// Operadores de Comparação
+
+console.log("\n=== OPERADORES DE COMPARAÇÃO ===");
+console.log(">", "Maior Que");
+console.log("<", "Menor Que");
+console.log(">=", "Maior ou Igual");
+console.log("<=", "Menor ou Igual");
+console.log("==", "Igual a");
+console.log("===", "Igualdade Estrita");
+console.log("!=", "Diferente");
+
+
+// Operadores Lógicos
+
+console.log("\n=== OPERADORES LÓGICOS ===");
+console.log("&&", "E");
+console.log("||", "OU");
+console.log("!", "NÃO");
