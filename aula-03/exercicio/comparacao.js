99b5730bfdcf58bedc5a09afd8ef596e4859bdbd
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-02-14 22:13:25 -0300

    Adicionado Exercício da Aula 03

---
+++ aula-03/exercicio/comparacao.js
@@ -0,0 +1,5 @@
+function comparar() {
+    const num1 = parseInt(document.getElementById("num1").value);
+    const num2 = parseInt(document.getElementById("num2").value);
+    document.getElementById("resultado").value = num1 > num2;
+}
