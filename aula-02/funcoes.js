dbbc325a30d3fdf9236a652901c94f84d01007f3
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-03-04 22:12:21 -0300

    Adicionado Arrow Function

--- aula-02/funcoes.js
+++ aula-02/funcoes.js
@@ -21,3 +21,23 @@ console.log(sum); // Referência à função
 console.log(sum()); // 0
 console.log(sum(5)); // 5
 console.log(sum(5, 10)); // 15
+
+
+// Arrow Function
+console.log("=== ARROW FUNCTION ===");
+
+let numbers = [1, 2, 3];
+
+// DE: function(valor1) { return valor2 }
+// PARA: (valor1) => { return valor2 }
+
+// numbers.filter(function(valor) {
+numbers = numbers.filter( (valor) => {
+    return valor < 3;
+});
+
+// DE: function(valor1) { return valor2 }
+// PARA: valor1 => valor2;
+numbers = numbers.filter(valor => valor < 3);
+
+console.log(numbers);
