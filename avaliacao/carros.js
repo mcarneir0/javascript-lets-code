91ff4d7f1e322b3b6e566056a38d47b701e1e123
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-03-06 23:01:39 -0300

    Adicionado Preço no input e na tabela

--- avaliacao/carros.js
+++ avaliacao/carros.js
@@ -3,13 +3,22 @@ const tabela = document.getElementById("tabela");
 let id = 1;
 
 function adicionar() {
+
+    const modelo = document.getElementById("modelo").value;
+    const ano = document.getElementById("ano").value;
+    const cor = document.getElementById("cor").value;
+    const situacao = document.getElementById("situacao").value;
+
     const carro = {
         id: id++,
-        modelo: document.getElementById("modelo").value,
-        ano: document.getElementById("ano").value,
-        cor: document.getElementById("cor").value,
-        situacao: document.getElementById("situacao").value,
-    }
+        modelo: modelo,
+        ano: ano,
+        cor: cor,
+        situacao: situacao
+    };
+    if (document.getElementById("preco").value == 0) { carro.preco = "R$ 0"}
+    else {carro.preco = "R$ " + document.getElementById("preco").value};
+    
     carros.push(carro);
     mostra_tabela();
     atualiza_pagina();
@@ -25,6 +34,7 @@ function mostra_tabela() {
                     <td>${carro.ano}</td>
                     <td>${carro.cor}</td>
                     <td>${carro.situacao}</td>
+                    <td>${carro.preco}</td>
                 </tbody>`;
             return dados;
         }
@@ -34,17 +44,27 @@ function mostra_tabela() {
 function atualiza_pagina() {
     document.getElementById("modelo").value = "";
     document.getElementById("ano").value = "";
+    document.getElementById("preco").value = "";
     document.getElementById("cor").value = "#000000";
     document.getElementById("situacao").value = "novo";
     document.getElementById("modelo").focus();
+}
+
+//  Adiciona pontuação no campo preço. ex: 123,456.78
+document.getElementById("preco").onblur = function () {    
+    this.value = parseFloat(this.value.replace(/,/g, "."))
+                    .toFixed(2)
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    document.getElementById("preco").value = this.value;
 }
 
 /*  TODO Avaliação
 
     [X] copiar exercicio da loja de carros
     [X] add uma coluna com um id (ao lado esquerdo do modelo)
-    [] add no input um campo para receber o preço
-    [] exibir uma coluna com o preço
+    [X] add no input um campo para receber o preço
+    [X] exibir uma coluna com o preço
     [] exibir o valor total de todos os veículos cadastrados ( usar reduce)
 
 */
